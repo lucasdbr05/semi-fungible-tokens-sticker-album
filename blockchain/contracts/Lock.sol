b96af101acc834f9bfc673705f9bf525be9d65a5
lucasdbr05 <lucasgabrieljake@gmail.com>
2025-12-09 12:01:14 -0300

    refactor: minor changes

--- blockchain/contracts/Lock.sol
+++ blockchain/contracts/Lock.sol
@@ -1,7 +1,5 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// Import OpenZeppelin implementations
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
